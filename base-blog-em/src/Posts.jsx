a628195f8a653172bfbeb95de67447469fbf853f
choi2601 <byeonghyeonchoe55@gmail.com>
2023-01-06 22:09:26 +0900

    로딩 상태와 에러 상태 처리하기

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -16,10 +16,20 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const { data } = useQuery("posts", fetchPosts);
+  // isFetching: 비동기 쿼리가 해결(resolved)되지 않았음을 의미
+  // isLoading: isFetching의 하위 집합. 캐싱된 데이터도 없고 쿼리를 만든 적도 없는 경우를 의미
 
-  if (!data) return <div>no data</div>;
+  // 기본적으로 react-query는 fetch 시도를 3번 함
+  const { data, isError, error, isLoading } = useQuery("posts", fetchPosts);
+
+  if (isLoading) return <h3>loading...</h3>;
+  if (isError)
+    return (
+      <>
+        <h3>Oops, someting went wrong</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
 
   return (
     <>
