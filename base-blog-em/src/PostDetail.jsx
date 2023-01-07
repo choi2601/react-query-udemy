44f7a7da1bc6dab66cc4ed0d7819a834207d90cb
choi2601 <byeonghyeonchoe55@gmail.com>
2023-01-07 14:31:03 +0900

    useMutation로 포스팅 삭제하기

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery, useMutation } from "react-query";
 
 async function fetchComments(postId) {
   const response = await fetch(
@@ -29,6 +29,7 @@ export function PostDetail({ post }) {
     ["comments", post.id],
     () => fetchComments(post.id)
   );
+  const deleteMutation = useMutation((postId) => deletePost(postId));
 
   if (isLoading) {
     return <h3>Loading..</h3>;
@@ -46,7 +47,19 @@ export function PostDetail({ post }) {
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button onClick={() => deleteMutation.mutate(post.id)}>
+        Delete
+      </button>{" "}
+      {deleteMutation.error && (
+        <p style={{ color: "red" }}>Error deleting the post</p>
+      )}
+      {deleteMutation.isLoading && (
+        <p style={{ color: "purple" }}>Deleting the post</p>
+      )}
+      {deleteMutation.isSuccess && (
+        <p style={{ color: "green" }}>Post has been deleted</p>
+      )}
+      <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
