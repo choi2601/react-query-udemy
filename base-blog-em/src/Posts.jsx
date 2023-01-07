5a47f0d36f608e1dcf60367bd08c6004801622aa
choi2601 <byeonghyeonchoe55@gmail.com>
2023-01-07 14:05:26 +0900

    페이지 매김(Pagination)

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -5,15 +5,15 @@ import { useQuery } from "react-query";
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(pageNum) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
   // isFetching: 비동기 쿼리가 해결(resolved)되지 않았음을 의미
@@ -37,9 +37,13 @@ export function Posts() {
 
   // 캐시가 만료되면 가비지 컬렉션이 실행되고 클라이언트는 데이터를 사용할 수 없게 됨(빈 화면을 보게 될 수 있음)
 
-  const { data, isError, error, isLoading } = useQuery("posts", fetchPosts, {
-    staleTime: 2000,
-  });
+  const { data, isError, error, isLoading } = useQuery(
+    ["posts", currentPage],
+    () => fetchPosts(currentPage),
+    {
+      staleTime: 2000,
+    }
+  );
 
   if (isLoading) return <h3>loading...</h3>;
   if (isError)
@@ -64,11 +68,21 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((prev) => prev - 1);
+          }}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((prev) => prev + 1);
+          }}
+        >
           Next page
         </button>
       </div>
