4e3ebf3abe76fb42d18a36dfd91cc709add76f6c
choi2601 <byeonghyeonchoe55@gmail.com>
2023-05-13 17:54:24 +0900

    직원 필터링 테스트

--- base-lazy-days/client/src/components/staff/tests/useStaff.test.tsx
+++ base-lazy-days/client/src/components/staff/tests/useStaff.test.tsx
@@ -1,5 +1,20 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { act } from 'react-dom/test-utils';
+import { createQueryClientWrapper } from 'test-utils';
+
 import { useStaff } from '../hooks/useStaff';
 
 test('filter staff', async () => {
-  // the magic happens here
+  const { result, waitFor } = renderHook(useStaff, {
+    wrapper: createQueryClientWrapper(),
+  });
+
+  // wait for staff to populate
+  await waitFor(() => result.current.staff.length === 4);
+
+  // set to filter for only staff who give masssage
+  act(() => result.current.setFilter('facial'));
+
+  // wait for the staff list to display only 3
+  await waitFor(() => result.current.staff.length === 3);
 });
