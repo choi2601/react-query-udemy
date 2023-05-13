dd8917f6ce0e8244f184ab4d0eefe83c965aef15
choi2601 <byeonghyeonchoe55@gmail.com>
2023-05-13 17:30:45 +0900

    예약 취소 변이 테스트하기

--- base-lazy-days/client/src/components/appointments/tests/appointmentMutations.test.tsx
+++ base-lazy-days/client/src/components/appointments/tests/appointmentMutations.test.tsx
@@ -41,9 +41,25 @@ test('Reserve appointment', async () => {
 });
 
 test('Cancel appointment', async () => {
-  // your test here
-  //
-  // const cancelButtons = await screen.findAllByRole('button', {
-  //   name: /cancel appointment/i,
-  //  });
+  renderWithQueryClient(
+    <MemoryRouter>
+      <Calendar />
+    </MemoryRouter>,
+  );
+
+  const cancelButtons = await screen.findAllByRole('button', {
+    name: /cancel appointments/i,
+  });
+
+  // click on the first one to cancel
+  fireEvent.click(cancelButtons[0]);
+
+  // check for the toast alert
+  const alertToast = await screen.findByRole('alert');
+  expect(alertToast).toHaveTextContent('cancel');
+
+  // close alert to keep state clean and wait for it to disappear
+  const alertCloseButton = screen.getByRole('button', { name: 'close' });
+  alertCloseButton.click();
+  await waitForElementToBeRemoved(alertToast);
 });
